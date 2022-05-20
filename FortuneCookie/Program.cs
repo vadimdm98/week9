1cdda9719173d96d5b492f4cc6b3d4d542f819ee
vadimdm98 <101522820+vadimdm98@users.noreply.github.com>
2022-05-20 14:37:23 +0300

    Add files via upload

---
+++ FortuneCookie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FortuneCookie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] furtunes = new string[]
+{
+  "win a Jackpot",
+  "fall in love",
+  "get a love letter",
+  "miss the bus",
+  "see a unicorn un the street"
+};
+
+            Console.WriteLine("Your cookie is: {0}", furtunes.GetValue(new Random().Next(furtunes.Length)));
+        }
+    }
+}
---
+++ MatterOfTaste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MatterOfTaste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = new string[]
+{
+  "red",
+  "blue",
+  "green"
+};
+
+            Console.Write("Hi, enter your favorite color: ");
+
+            string userColor = Console.ReadLine().ToLower();
+
+            if (Array.Exists(colors, color => color.Equals(userColor)))
+            {
+                Console.WriteLine("We are perfect together.");
+            }
+            else
+            {
+                Console.WriteLine("Every man to his taste.");
+            }
+        }
+    }
+}
